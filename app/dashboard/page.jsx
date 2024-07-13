f5ace34e45839b88816a9f5b65bba3f8476519c2
Muhammad Chandra Ramadhan <82149114+chandraramadhan23@users.noreply.github.com>
2024-07-13 11:37:47 +0700

    feat: first dashboard page

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const dashboardPage = () => {
+    return (
+        <div>
+            <h1>Dashboard Page</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita nulla, ipsum minus placeat sunt illo nesciunt, praesentium rerum iusto, in ad doloribus vitae facilis. Doloremque atque adipisci minus officia dolores.</p>
+        </div>
+    );
+}
+
+export default dashboardPage
